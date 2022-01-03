12fc30336528e1986e19ca95645644c2cc9eb290
yotsufe <yotsufe@gmail.com>
2022-01-03 14:34:31 +0900

    Section 3: Support Editable Notes

--- Today/Views/EditNotesCell.swift
+++ Today/Views/EditNotesCell.swift
@@ -1,9 +1,29 @@
 import UIKit
 
 class EditNotesCell: UITableViewCell {
-    @IBOutlet var notesTextView: UITextField!
+    typealias NotesChangeAction = (String) -> Void
 
-    func configure(notes: String?) {
+    @IBOutlet var notesTextView: UITextView!
+
+    private var notesChangeAction: NotesChangeAction?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        notesTextView.delegate = self
+    }
+
+    func configure(notes: String?, changeAction: NotesChangeAction?) {
         notesTextView.text = notes
+        self.notesChangeAction = changeAction
+    }
+}
+
+extension EditNotesCell: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if let originalText = textView.text {
+            let title = (originalText as NSString).replacingCharacters(in: range, with: text)
+            notesChangeAction?(title)
+        }
+        return true
     }
 }
